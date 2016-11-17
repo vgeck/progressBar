37e486da0bce5d39ab60649ad036b2f6f3a80db6
Vinzenz Gregor Eck <vinzenz@eck-mail.eu>
2016-11-17 11:53:14 +0100

    renamed package to progressbarsimple

--- setup.py
+++ setup.py
@@ -7,12 +7,17 @@ Installer of progressbar-simple
 
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name='progressbar-simple',
       version='0.1',
       description='Another simple progress bar',
       url='http://github.com/vgeck/progressBar',
       author='Vinzenz Eck',
-      author_email='vinzenzeck@mytum.de',
+      author_email='vinzenz@xal.no',
       license='GNU3',
-      packages=['progressbar-simple'],
+      long_description=readme(),
+      packages=['progressbarsimple'],
       zip_safe=False)
