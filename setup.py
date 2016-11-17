9762d436c29bc29b5b5e9546a083e1a55e598391
Vinzenz Gregor Eck <vinzenz@eck-mail.eu>
2016-11-17 14:24:41 +0100

    added setup.cfg, manifest.ini, included keywords and categories in setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,20 @@ Installer of progressbar-simple
 from setuptools import setup
 
 def readme():
-    with open('README.md') as f:
+    with open('README.rst') as f:
         return f.read()
 
 setup(name='progressbar-simple',
       version='0.1',
       description='Another simple progress bar',
+      classifiers = [ 	'Development Status :: 4 - Beta',
+      			'Intended Audience :: Developers',
+			'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+			'Natural Language :: English',
+			'Programming Language :: Python :: 2.7',
+			'Programming Language :: Python :: 3.5',
+			'Topic :: Utilities' ],
+      keywords='terminal process bar',
       url='http://github.com/vgeck/progressBar',
       author='Vinzenz Eck',
       author_email='vinzenz@xal.no',
