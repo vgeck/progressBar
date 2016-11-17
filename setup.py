89e29cfcee1f552a4a07d768616b84c59c0c7b26
Vinzenz Gregor Eck <vinzenz@eck-mail.eu>
2016-11-17 11:27:14 +0100

    added setup.py file and mv progressBar.py to progressbar-simple/__init.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf8
+
+"""
+Installer of progressbar-simple
+"""
+
+from setuptools import setup
+
+setup(name='progressbar-simple',
+      version='0.1',
+      description='Another simple progress bar',
+      url='http://github.com/vgeck/progressBar',
+      author='Vinzenz Eck',
+      author_email='vinzenzeck@mytum.de',
+      license='GNU3',
+      packages=['progressbar-simple'],
+      zip_safe=False)
