92dd1a0d72e8b4c856960fac0c06e21a391be82f
Vinzenz Gregor Eck <vinzenz@eck-mail.eu>
2016-11-21 17:00:06 +0100

    updated example files

--- example1.py
+++ example1.py
@@ -2,7 +2,7 @@
 # encoding: utf-8
 
 # for python 2.7
-from __future__ import print_function  
+from __future__ import print_function
 from __future__ import unicode_literals
 from builtins import range
 
@@ -14,26 +14,32 @@ print("\nexample 1:")
 print("a simple progress bar showing the progress of a silly 'for'-loop \n\n")
 
 
-print("'for'-loop without subpressing the print inside the 'for'-loop out: \n")
+print("'for'-loop without suppressing the print inside the 'for'-loop out: \n")
 # initializing progress bar
-progressBar = ProgressBar(nElements = 35, nIterations = 10, subpressPrint = False)
+progressBar = ProgressBar(nElements=10, nIterations=10, suppressPrint=False)
+
+progressBar.showEmptyBar()
+print("This message will be shown right away")
 
 for i in range(10):
-    time.sleep(random.random()*0.4)
-    # as the print out is no subpressed the printout is shown after right away
-    # and the process bar is moved down
+    time.sleep(random.random() * 0.4)
+    # as the printout is not suppressed the printout is shown right away
+    # and the progress bar is moved down
     print("eval {}".format(i))
-    # feed the progress bar with the necessary progress information
-    progressBar.progress(i)
-    
-print("\n'for'-loop subpressing the print inside the 'for'-loop out: \n")
+    # tell the progress bar that the next iteration was done
+    progressBar.progress()
+
+print("\n'for'-loop suppressing the print inside the 'for'-loop out: \n")
 # initializing progress bar
-progressBar = ProgressBar(nElements = 35, nIterations = 10, subpressPrint = True)
+progressBar = ProgressBar(nElements=35, nIterations=10, suppressPrint=True)
+
+progressBar.showEmptyBar()
+print("This message will be shown at the end")
 
 for i in range(10):
-    time.sleep(random.random()*0.4)
-    # as stdout is redirected print outs here will be saved and 
+    time.sleep(random.random() * 0.4)
+    # as the printout is not suppressed, the printouts are saved and
     # shown after the for-loop is finished
     print("eval {}".format(i))
-    # feed the progress bar with the necessary progress information
-    progressBar.progress(i)
+    # tell the progress bar that the next iteration was done
+    progressBar.progress()
--- example2.py
+++ example2.py
@@ -3,7 +3,7 @@
 
 
 # for python 2.7
-from __future__ import print_function  
+from __future__ import print_function
 from __future__ import unicode_literals
 from builtins import range
 
@@ -13,23 +13,23 @@ import multiprocessing, time
 def foo(uselessInput):
     """
     Function foo running a silly for loop to waste some time
-    
+
     Args:
     uselessInput (any): not used input, just needed for the pool map function
-    
+
     Returns:
         minutesSolve: minutes needed for iterating through the for-loop
         secsSolve: seconds needed for iterating through the for-loop
     """
     timeStart = time.clock()
     for i in range(1000):
-        i+100*i
+        i + 100 * i
     time.sleep(0.2)
-    timeSolverSolve = time.clock()-timeStart
-    minutesSolve = int(timeSolverSolve/60.)
-    secsSolve = timeSolverSolve-minutesSolve*60.
-        
-    return minutesSolve,secsSolve
+    timeSolverSolve = time.clock() - timeStart
+    minutesSolve = int(timeSolverSolve / 60.)
+    secsSolve = timeSolverSolve - minutesSolve * 60.
+
+    return minutesSolve, secsSolve
 
 
 # start timing of the hole process
@@ -41,12 +41,12 @@ numberWorkers = multiprocessing.cpu_count()
 # create a multiprocess iterator named "results"
 pool = multiprocessing.Pool(numberWorkers)
 results = pool.imap_unordered(foo, batchDataList)
-pool.close() 
+pool.close()
 
-# create a loading bar 
+# create a loading bar
 nElements = 50
 nSteps = len(batchDataList)
-progressBar = ProgressBar(nElements, nSteps, subpressPrint = False)
+progressBar = ProgressBar(nElements, nSteps, suppressPrint=False)
 
 print('=====================================')
 print('------Multiprocessing Batch Job------')
@@ -54,23 +54,24 @@ print('numberWorkers:   {}'.format(numberWorkers))
 print('numberOfEval.:   {}'.format(len(batchDataList)))
 
 # run the batch jobs
-while (True):
-    completed = results._index
-    # feed the progress bar with the necessary progress information
-    progressBar.progress(completed)
-    if (completed == len(batchDataList)): break
+completed = 0
+while completed < nSteps:
+    if completed < results._index:
+        completed = results._index
+    progressBar.progressMultiprocessing(results._index)
+
 pool.join()
 
 # print the results of all the batch simulations
 print('=====================================')
-for batchJobIndex,[minutesSolve,secsSolve] in enumerate(results):
-    print('____________Batch   {:5} ___________'.format(batchJobIndex+1))
-    print('Runtime:        {} min {} sec'.format(minutesSolve,secsSolve))
+for batchJobIndex, [minutesSolve, secsSolve] in enumerate(results):
+    print('____________Batch   {:5} ___________'.format(batchJobIndex + 1))
+    print('Runtime:        {} min {} sec'.format(minutesSolve, secsSolve))
 print('=====================================')
 
 # print the time needed for the hole process
-timeBatchJob= time.time()-timeStartBatch
-minutesBatch = int(timeBatchJob/60.)
-secsBatch = timeBatchJob-minutesBatch*60.
-print('total runtime:  {} min {} sec'.format(minutesBatch,secsBatch))
+timeBatchJob = time.time() - timeStartBatch
+minutesBatch = int(timeBatchJob / 60.)
+secsBatch = timeBatchJob - minutesBatch * 60.
+print('total runtime:  {} min {} sec'.format(minutesBatch, secsBatch))
 print('=====================================')
